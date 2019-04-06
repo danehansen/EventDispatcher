27b3f7be95eec91c26e0868f4615bcee751847a4
Dane Hansen <dane@danehansen.com>
2019-04-05 18:31:43 -0700

    use package-helpers

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# EventDispatcher
+# EventDispatcher ![npm bundle size (scoped)](https://img.shields.io/bundlephobia/minzip/@danehansen/event-dispatcher.svg) ![npm](https://img.shields.io/npm/dt/@danehansen/event-dispatcher.svg)
 
-__Class__ : public class [EventDispatcher](https://github.com/danehansen/EventDispatcher)  
-__Inheritance__ : [EventDispatcher](https://github.com/danehansen/EventDispatcher) > Object  
-__Subclasses__ : [Preloader](https://github.com/danehansen/Preloader), [Sprite](https://github.com/danehansen/Sprite), [Timer](https://github.com/danehansen/Timer)
+**Class** : public class [EventDispatcher](https://github.com/danehansen/EventDispatcher)  
+**Inheritance** : [EventDispatcher](https://github.com/danehansen/EventDispatcher) > Object  
+**Subclasses** : [Preloader](https://github.com/danehansen/Preloader), [Sprite](https://github.com/danehansen/Sprite), [Timer](https://github.com/danehansen/Timer)
 
 The EventDispatcher class is the base class for all classes that dispatch events. It is totally ripped off from the AS3 EventDispatcher, but simplified. EventDispatcher can also be used without creating an instance like singleton emitter.
 
@@ -39,22 +39,22 @@ In your browser:
 
 ## Public Static Methods
 
-* __addEventListener__(type:String, listener:Function)  
-[static] Registers an event listener object with EventDispatcher so that it will dispatch notification of an event.
-* __dispatchEvent__(type:String, ...args:*)  
-[static] Emits an event, and passes along any extra arguments to the registered listeners.
-* __removeEventListener__ (type:String, listener:Function)  
-[static] Removes a listener from EventDispatcher.
+- **addEventListener**(type:String, listener:Function)  
+  [static] Registers an event listener object with EventDispatcher so that it will dispatch notification of an event.
+- **dispatchEvent**(type:String, ...args:\*)  
+  [static] Emits an event, and passes along any extra arguments to the registered listeners.
+- **removeEventListener** (type:String, listener:Function)  
+  [static] Removes a listener from EventDispatcher.
 
 ## Public Methods
 
-* __EventDispatcher__(target:Object)  
-Aggregates an instance of the EventDispatcher class.
-* __addEventListener__(type:String, listener:Function)  
-Registers an event listener object with an EventDispatcher object so that the listener receives notification of an event.
-* __clearEventListeners__()  
-Clears all event listeners of the instance.
-* __dispatchEvent__(type:String)  
-Dispatches an event into the event flow.
-* __removeEventListener__ (type:String, listener:Function)  
-Removes a listener from the EventDispatcher object.
+- **EventDispatcher**(target:Object)  
+  Aggregates an instance of the EventDispatcher class.
+- **addEventListener**(type:String, listener:Function)  
+  Registers an event listener object with an EventDispatcher object so that the listener receives notification of an event.
+- **clearEventListeners**()  
+  Clears all event listeners of the instance.
+- **dispatchEvent**(type:String)  
+  Dispatches an event into the event flow.
+- **removeEventListener** (type:String, listener:Function)  
+  Removes a listener from the EventDispatcher object.
--- test/test.js
+++ test/test.js
@@ -1,254 +1,278 @@
-import EventDispatcher from '../src/EventDispatcher'
-import { expect } from 'chai'
-import { spy } from 'sinon'
+import EventDispatcher from "../src/EventDispatcher";
+import { expect } from "chai";
+import { spy } from "sinon";
+import fs from "fs";
+import path from "path";
 
-describe('EventDispatcher', function() {
-  describe('constructor', function() {
-    it('sets target to self by default', function() {
-      const ed = new EventDispatcher()
-      expect(ed._target).to.equal(ed)
-      expect(ed._callbacks).to.deep.equal({})
-    })
+describe("EventDispatcher", function() {
+  describe("danehansen-EventDispatcher.min.js", function() {
+    it("is minified", function() {
+      const min = fs.readFileSync(
+        path.join(__dirname, "../danehansen-EventDispatcher.min.js"),
+        "utf8"
+      );
+      expect(min.match(/\n/g)).to.be.null;
+    });
+  });
 
-    it('sets target to argument', function() {
-      const t = {}
-      const ed = new EventDispatcher(t)
-      expect(ed._target).to.equal(t)
-      expect(ed._callbacks).to.deep.equal({})
-    })
-  })
+  describe("constructor", function() {
+    it("sets target to self by default", function() {
+      const ed = new EventDispatcher();
+      expect(ed._target).to.equal(ed);
+      expect(ed._callbacks).to.deep.equal({});
+    });
 
-  describe('addEventListener', function() {
-    const ed = new EventDispatcher()
-    const eventA = 'a'
-    const eventB = 'b'
-    const callbackA = spy()
-    const callbackB = spy()
-    const callbackC = spy()
+    it("sets target to argument", function() {
+      const t = {};
+      const ed = new EventDispatcher(t);
+      expect(ed._target).to.equal(t);
+      expect(ed._callbacks).to.deep.equal({});
+    });
+  });
 
-    it('adds callbacks', function() {
-      ed.addEventListener(eventA, callbackA)
-      ed.addEventListener(eventB, callbackB)
-      ed.addEventListener(eventA, callbackC)
-      ed.addEventListener(eventB, callbackC)
+  describe("addEventListener", function() {
+    const ed = new EventDispatcher();
+    const eventA = "a";
+    const eventB = "b";
+    const callbackA = spy();
+    const callbackB = spy();
+    const callbackC = spy();
+
+    it("adds callbacks", function() {
+      ed.addEventListener(eventA, callbackA);
+      ed.addEventListener(eventB, callbackB);
+      ed.addEventListener(eventA, callbackC);
+      ed.addEventListener(eventB, callbackC);
 
       expect(ed._callbacks).to.deep.equal({
         [eventA]: [callbackA, callbackC],
-        [eventB]: [callbackB, callbackC],
-      })
-    })
+        [eventB]: [callbackB, callbackC]
+      });
+    });
 
-    it('doesn‘t add duplicate callbacks', function() {
-      ed.addEventListener(eventA, callbackA)
-      ed.addEventListener(eventA, callbackA)
-      ed.addEventListener(eventB, callbackB)
-      ed.addEventListener(eventB, callbackB)
+    it("doesn‘t add duplicate callbacks", function() {
+      ed.addEventListener(eventA, callbackA);
+      ed.addEventListener(eventA, callbackA);
+      ed.addEventListener(eventB, callbackB);
+      ed.addEventListener(eventB, callbackB);
 
       expect(ed._callbacks).to.deep.equal({
         [eventA]: [callbackA, callbackC],
-        [eventB]: [callbackB, callbackC],
-      })
-    })
-  })
+        [eventB]: [callbackB, callbackC]
+      });
+    });
+  });
 
-  describe('removeEventListener', function() {
-    const ed = new EventDispatcher()
-    const eventA = 'a'
-    const eventB = 'b'
-    const callbackA = spy()
-    const callbackB = spy()
-    const callbackC = spy()
-    ed.addEventListener(eventA, callbackA)
-    ed.addEventListener(eventB, callbackB)
-    ed.addEventListener(eventA, callbackC)
-    ed.addEventListener(eventB, callbackC)
+  describe("removeEventListener", function() {
+    const ed = new EventDispatcher();
+    const eventA = "a";
+    const eventB = "b";
+    const callbackA = spy();
+    const callbackB = spy();
+    const callbackC = spy();
+    ed.addEventListener(eventA, callbackA);
+    ed.addEventListener(eventB, callbackB);
+    ed.addEventListener(eventA, callbackC);
+    ed.addEventListener(eventB, callbackC);
 
-    it('removes callbacks', function() {
-      ed.removeEventListener(eventA, callbackA)
-
-      expect(ed._callbacks).to.deep.equal({
-        [eventA]: [callbackC],
-        [eventB]: [callbackB, callbackC],
-      })
-    })
-
-    it('handles removal of nonexistant callbacks', function() {
-      ed.removeEventListener(eventA, callbackA)
+    it("removes callbacks", function() {
+      ed.removeEventListener(eventA, callbackA);
 
       expect(ed._callbacks).to.deep.equal({
         [eventA]: [callbackC],
-        [eventB]: [callbackB, callbackC],
-      })
-    })
-  })
+        [eventB]: [callbackB, callbackC]
+      });
+    });
 
-  describe('clearEventListeners', function() {
-    const ed = new EventDispatcher()
-    const eventA = 'a'
-    const eventB = 'b'
-    const callbackA = spy()
-    const callbackB = spy()
-    const callbackC = spy()
-    ed.addEventListener(eventA, callbackA)
-    ed.addEventListener(eventB, callbackB)
-    ed.addEventListener(eventA, callbackC)
-    ed.addEventListener(eventB, callbackC)
+    it("handles removal of nonexistant callbacks", function() {
+      ed.removeEventListener(eventA, callbackA);
 
-    it('removes all listeners', function() {
-      ed.clearEventListeners()
+      expect(ed._callbacks).to.deep.equal({
+        [eventA]: [callbackC],
+        [eventB]: [callbackB, callbackC]
+      });
+    });
+  });
 
-      expect(ed._callbacks).to.deep.equal({})
-    })
-  })
+  describe("clearEventListeners", function() {
+    const ed = new EventDispatcher();
+    const eventA = "a";
+    const eventB = "b";
+    const callbackA = spy();
+    const callbackB = spy();
+    const callbackC = spy();
+    ed.addEventListener(eventA, callbackA);
+    ed.addEventListener(eventB, callbackB);
+    ed.addEventListener(eventA, callbackC);
+    ed.addEventListener(eventB, callbackC);
 
-  describe('dispatchEvent', function() {
-    const ed = new EventDispatcher()
-    const eventA = 'a'
-    const eventB = 'b'
-    const callbackA = spy()
-    const callbackB = spy()
-    const callbackC = spy()
-    const extraParamA = 'aa'
-    const extraParamB = 'bb'
-    ed.addEventListener(eventA, callbackA)
-    ed.addEventListener(eventB, callbackB)
-    ed.addEventListener(eventA, callbackC)
-    ed.addEventListener(eventB, callbackC)
+    it("removes all listeners", function() {
+      ed.clearEventListeners();
 
-    it('calls proper callback', function() {
-      ed.dispatchEvent(eventA, extraParamA, extraParamB)
+      expect(ed._callbacks).to.deep.equal({});
+    });
+  });
 
-      expect(callbackA.callCount).to.equal(1)
-      expect(callbackB.callCount).to.equal(0)
-      expect(callbackC.callCount).to.equal(1)
-    })
+  describe("dispatchEvent", function() {
+    const ed = new EventDispatcher();
+    const eventA = "a";
+    const eventB = "b";
+    const callbackA = spy();
+    const callbackB = spy();
+    const callbackC = spy();
+    const extraParamA = "aa";
+    const extraParamB = "bb";
+    ed.addEventListener(eventA, callbackA);
+    ed.addEventListener(eventB, callbackB);
+    ed.addEventListener(eventA, callbackC);
+    ed.addEventListener(eventB, callbackC);
 
-    it('doesnt’t call after removal', function() {
-      ed.clearEventListeners()
-      ed.dispatchEvent(eventA)
+    it("calls proper callback", function() {
+      ed.dispatchEvent(eventA, extraParamA, extraParamB);
 
-      expect(callbackA.callCount).to.equal(1)
-      expect(callbackB.callCount).to.equal(0)
-      expect(callbackC.callCount).to.equal(1)
-    })
+      expect(callbackA.callCount).to.equal(1);
+      expect(callbackB.callCount).to.equal(0);
+      expect(callbackC.callCount).to.equal(1);
+    });
 
-    it('calls with proper arguments', function() {
-      expect(callbackA.calledWith({
-        target: ed,
-        type: eventA,
-      }, extraParamA, extraParamB)).to.equal(true)
-    })
+    it("doesnt’t call after removal", function() {
+      ed.clearEventListeners();
+      ed.dispatchEvent(eventA);
 
-    it('calls with another target when specified', function() {
-      const obj = {}
-      const ed2 = new EventDispatcher(obj)
-      ed2.addEventListener(eventB, callbackB)
-      ed2.dispatchEvent(eventB, extraParamA, extraParamB)
+      expect(callbackA.callCount).to.equal(1);
+      expect(callbackB.callCount).to.equal(0);
+      expect(callbackC.callCount).to.equal(1);
+    });
 
-      expect(callbackB.calledWith({
-        target: obj,
-        type: eventB,
-      }, extraParamA, extraParamB)).to.equal(true)
-    })
-  })
+    it("calls with proper arguments", function() {
+      expect(
+        callbackA.calledWith(
+          {
+            target: ed,
+            type: eventA
+          },
+          extraParamA,
+          extraParamB
+        )
+      ).to.equal(true);
+    });
 
-  describe('static addEventListener', function() {
-    const eventA = 'a'
-    const eventB = 'b'
-    const callbackA = spy()
-    const callbackB = spy()
-    const callbackC = spy()
+    it("calls with another target when specified", function() {
+      const obj = {};
+      const ed2 = new EventDispatcher(obj);
+      ed2.addEventListener(eventB, callbackB);
+      ed2.dispatchEvent(eventB, extraParamA, extraParamB);
 
-    it('adds callbacks', function() {
-      EventDispatcher.addEventListener(eventA, callbackA)
-      EventDispatcher.addEventListener(eventB, callbackB)
-      EventDispatcher.addEventListener(eventA, callbackC)
-      EventDispatcher.addEventListener(eventB, callbackC)
-      EventDispatcher.dispatchEvent(eventA)
+      expect(
+        callbackB.calledWith(
+          {
+            target: obj,
+            type: eventB
+          },
+          extraParamA,
+          extraParamB
+        )
+      ).to.equal(true);
+    });
+  });
 
-      expect(callbackA.callCount).to.equal(1)
-      expect(callbackB.callCount).to.equal(0)
-      expect(callbackC.callCount).to.equal(1)
-    })
+  describe("static addEventListener", function() {
+    const eventA = "a";
+    const eventB = "b";
+    const callbackA = spy();
+    const callbackB = spy();
+    const callbackC = spy();
 
-    it('doesn‘t add duplicate callbacks', function() {
-      EventDispatcher.addEventListener(eventA, callbackA)
-      EventDispatcher.addEventListener(eventA, callbackA)
-      EventDispatcher.dispatchEvent(eventA)
+    it("adds callbacks", function() {
+      EventDispatcher.addEventListener(eventA, callbackA);
+      EventDispatcher.addEventListener(eventB, callbackB);
+      EventDispatcher.addEventListener(eventA, callbackC);
+      EventDispatcher.addEventListener(eventB, callbackC);
+      EventDispatcher.dispatchEvent(eventA);
 
-      expect(callbackA.callCount).to.equal(2)
-      expect(callbackB.callCount).to.equal(0)
-      expect(callbackC.callCount).to.equal(2)
-    })
-  })
+      expect(callbackA.callCount).to.equal(1);
+      expect(callbackB.callCount).to.equal(0);
+      expect(callbackC.callCount).to.equal(1);
+    });
 
-  describe('static removeEventListener', function() {
-    const eventA = 'a'
-    const eventB = 'b'
-    const callbackA = spy()
-    const callbackB = spy()
-    const callbackC = spy()
+    it("doesn‘t add duplicate callbacks", function() {
+      EventDispatcher.addEventListener(eventA, callbackA);
+      EventDispatcher.addEventListener(eventA, callbackA);
+      EventDispatcher.dispatchEvent(eventA);
 
-    it('removes callbacks', function() {
-      EventDispatcher.addEventListener(eventA, callbackA)
-      EventDispatcher.addEventListener(eventB, callbackB)
-      EventDispatcher.addEventListener(eventA, callbackC)
-      EventDispatcher.addEventListener(eventB, callbackC)
-      EventDispatcher.dispatchEvent(eventA)
-      EventDispatcher.removeEventListener(eventA, callbackA)
-      EventDispatcher.removeEventListener(eventA, callbackC)
-      EventDispatcher.dispatchEvent(eventA)
+      expect(callbackA.callCount).to.equal(2);
+      expect(callbackB.callCount).to.equal(0);
+      expect(callbackC.callCount).to.equal(2);
+    });
+  });
 
-      expect(callbackA.callCount).to.equal(1)
-      expect(callbackB.callCount).to.equal(0)
-      expect(callbackC.callCount).to.equal(1)
-    })
+  describe("static removeEventListener", function() {
+    const eventA = "a";
+    const eventB = "b";
+    const callbackA = spy();
+    const callbackB = spy();
+    const callbackC = spy();
 
-    it('handles removal of nonexistant callbacks', function() {
-      EventDispatcher.removeEventListener(eventA, callbackA)
-      EventDispatcher.removeEventListener('x', ()=>{})
-      EventDispatcher.dispatchEvent(eventA)
+    it("removes callbacks", function() {
+      EventDispatcher.addEventListener(eventA, callbackA);
+      EventDispatcher.addEventListener(eventB, callbackB);
+      EventDispatcher.addEventListener(eventA, callbackC);
+      EventDispatcher.addEventListener(eventB, callbackC);
+      EventDispatcher.dispatchEvent(eventA);
+      EventDispatcher.removeEventListener(eventA, callbackA);
+      EventDispatcher.removeEventListener(eventA, callbackC);
+      EventDispatcher.dispatchEvent(eventA);
 
-      expect(callbackA.callCount).to.equal(1)
-      expect(callbackB.callCount).to.equal(0)
-      expect(callbackC.callCount).to.equal(1)
-    })
-  })
+      expect(callbackA.callCount).to.equal(1);
+      expect(callbackB.callCount).to.equal(0);
+      expect(callbackC.callCount).to.equal(1);
+    });
 
-  describe('static dispatchEvent', function() {
-    const eventA = 'aaa'
-    const eventB = 'bbb'
-    const callbackA = spy()
-    const callbackB = spy()
-    const callbackC = spy()
-    const extraParamA = 'aa'
-    const extraParamB = 'bb'
-    EventDispatcher.addEventListener(eventA, callbackA)
-    EventDispatcher.addEventListener(eventB, callbackB)
-    EventDispatcher.addEventListener(eventA, callbackC)
-    EventDispatcher.addEventListener(eventB, callbackC)
+    it("handles removal of nonexistant callbacks", function() {
+      EventDispatcher.removeEventListener(eventA, callbackA);
+      EventDispatcher.removeEventListener("x", () => {});
+      EventDispatcher.dispatchEvent(eventA);
 
-    it('calls proper callback', function() {
-      EventDispatcher.dispatchEvent(eventA, extraParamA, extraParamB)
+      expect(callbackA.callCount).to.equal(1);
+      expect(callbackB.callCount).to.equal(0);
+      expect(callbackC.callCount).to.equal(1);
+    });
+  });
 
-      expect(callbackA.callCount).to.equal(1)
-      expect(callbackB.callCount).to.equal(0)
-      expect(callbackC.callCount).to.equal(1)
-    })
+  describe("static dispatchEvent", function() {
+    const eventA = "aaa";
+    const eventB = "bbb";
+    const callbackA = spy();
+    const callbackB = spy();
+    const callbackC = spy();
+    const extraParamA = "aa";
+    const extraParamB = "bb";
+    EventDispatcher.addEventListener(eventA, callbackA);
+    EventDispatcher.addEventListener(eventB, callbackB);
+    EventDispatcher.addEventListener(eventA, callbackC);
+    EventDispatcher.addEventListener(eventB, callbackC);
 
-    it('doesnt’t call after removal', function() {
-      EventDispatcher.removeEventListener(eventA, callbackA)
-      EventDispatcher.removeEventListener(eventA, callbackC)
-      EventDispatcher.dispatchEvent(eventA, extraParamA, extraParamB)
-      EventDispatcher.dispatchEvent('y', extraParamA, extraParamB)
+    it("calls proper callback", function() {
+      EventDispatcher.dispatchEvent(eventA, extraParamA, extraParamB);
 
-      expect(callbackA.callCount).to.equal(1)
-      expect(callbackB.callCount).to.equal(0)
-      expect(callbackC.callCount).to.equal(1)
-    })
+      expect(callbackA.callCount).to.equal(1);
+      expect(callbackB.callCount).to.equal(0);
+      expect(callbackC.callCount).to.equal(1);
+    });
 
-    it('calls with proper arguments', function() {
-      expect(callbackA.calledWith(extraParamA, extraParamB)).to.equal(true)
-    })
-  })
-})
+    it("doesnt’t call after removal", function() {
+      EventDispatcher.removeEventListener(eventA, callbackA);
+      EventDispatcher.removeEventListener(eventA, callbackC);
+      EventDispatcher.dispatchEvent(eventA, extraParamA, extraParamB);
+      EventDispatcher.dispatchEvent("y", extraParamA, extraParamB);
+
+      expect(callbackA.callCount).to.equal(1);
+      expect(callbackB.callCount).to.equal(0);
+      expect(callbackC.callCount).to.equal(1);
+    });
+
+    it("calls with proper arguments", function() {
+      expect(callbackA.calledWith(extraParamA, extraParamB)).to.equal(true);
+    });
+  });
+});
